197d4f00d0c6940a1a93e8bcc818ea3e0dd190b7
Maxim Ageev <maxim.ageev@paxful.com>
2023-02-04 18:20:22 +0300

    Use latest version of softfineware/discriminator-default-normalizer

--- src/SerializerDiscriminatorDefaultBundle.php
+++ src/SerializerDiscriminatorDefaultBundle.php
@@ -3,9 +3,22 @@ declare(strict_types=1);
 
 namespace SoftFineWare\SerializerDiscriminatorDefaultBundle;
 
+use SoftFineWare\SerializerDiscriminatorDefault\DiscriminatorDefaultNormalizer;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class SerializerDiscriminatorDefaultBundle extends AbstractBundle
+final class SerializerDiscriminatorDefaultBundle extends AbstractBundle
 {
-
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->setDefinition(
+            DiscriminatorDefaultNormalizer::class,
+            new Definition(DiscriminatorDefaultNormalizer::class)
+        )->setAutoconfigured(true)
+            ->setAutowired(true)
+            ->setPublic(true)
+            ->addTag('serializer.normalizer', ['priority' => 999]);
+    }
 }
